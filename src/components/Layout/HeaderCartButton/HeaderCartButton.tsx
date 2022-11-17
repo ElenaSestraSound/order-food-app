2ec622f3b171fd3d84c20943b241ec43476eca47
elena.sestrasound <elena.sestrasound@gmail.com>
2022-11-17 16:09:23 +0100

    cart icon

--- src/components/Layout/HeaderCartButton/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.tsx
@@ -1,6 +1,6 @@
-import { StarIcon } from '@chakra-ui/icons';
-import { Button, Text } from '@chakra-ui/react';
+import { Button, Tag, Text } from '@chakra-ui/react';
 import * as React from 'react';
+import CartIcon from './CartIcon';
 
 export interface IHeaderCartButtonProps {
 }
@@ -8,10 +8,15 @@ export interface IHeaderCartButtonProps {
 export default function HeaderCartButton(props: IHeaderCartButtonProps) {
     return (
         <Button
-            leftIcon={<StarIcon />}
+            leftIcon={<CartIcon />}
             backgroundColor={'white'}
             color={'teal.500'}>
             Cart
+            <Tag
+                size='md'
+                backgroundColor={'teal.50'}
+                color={'teal.500'}
+                marginLeft='5px'>3</Tag>
         </Button>
     );
 }
