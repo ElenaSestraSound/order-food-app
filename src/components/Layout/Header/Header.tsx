6e77bae7185b31eba9938e3356fd4764f6817e57
elena.sestrasound <elena.sestrasound@gmail.com>
2022-11-17 15:41:05 +0100

    header

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,30 @@
+import { Box, Button, Flex, Heading, Image, Spacer } from '@chakra-ui/react';
+import * as React from 'react';
+import classes from './Header.module.css'
+
+export interface IHeaderProps {
+}
+
+export default function Header(props: IHeaderProps) {
+    return (
+        <React.Fragment>
+            <Box as='header'
+                className={classes.header}
+                backgroundColor='teal.500'>
+                <Heading as='h1' size='xl'>React Meals</Heading>
+                <Spacer />
+                <Button
+                    backgroundColor={'white'}
+                    color={'teal.500'}>
+                    Cart
+                </Button>
+            </Box>
+            <Box className={classes.main_image}>
+                <img
+                    src='https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg'
+                    alt='A table full of delicious food!'
+                />
+            </Box>
+        </React.Fragment>
+    );
+}
---
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+import * as React from 'react';
+
+export interface IHeaderCartButtonProps {
+}
+
+export default function HeaderCartButton(props: IHeaderCartButtonProps) {
+    return (
+        <Button>
+
+        </Button>
+    );
+}
