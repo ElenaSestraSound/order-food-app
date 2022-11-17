840b6a7f81d9dadbbe9a46691facc91510d0979f
elena.sestrasound <elena.sestrasound@gmail.com>
2022-11-17 17:13:09 +0100

    meals summary style

--- src/components/Meals/MealsSummary.tsx
+++ src/components/Meals/MealsSummary.tsx
@@ -1,15 +1,17 @@
 import { Box, Text } from '@chakra-ui/react';
 import * as React from 'react';
+import classes from './MealsSummary.module.css'
 
 export default function MealsSummary() {
     return (
-        <Box as='section'>
-            <Text as='h2'>Delicious Food, Delivered To You</Text>
-            <Text as='p'>
+        <Box as='section' className={classes.summary} backgroundColor={'teal'}>
+            <Text as='h2' fontSize='3xl' color={'white'}>Delicious Food, Delivered To You</Text>
+            <Text as='p' color={'white'}>
                 Choose your favourite meal from our abroad selection of available
                 meals and enjoy a delicious lunch or dinner at home.
             </Text>
-            <Text as='p'>
+            <br />
+            <Text as='p' color={'white'}>
                 All our meals are cooked with high-quality ingridients, just-in-time and
                 of course by experienced chefs!
             </Text>
